230915c20361619f383c871fb569be965a6963f4
Codayblue <codayblue@gmail.com>
2017-04-22 09:27:35 -0700

    Flushed out the linux script and included ohmyzsh curl to get it

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 if [ `uname -s` = 'Darwin' ]; then
     sh ./osx.sh
 else
-    echo "TODO"
+    sh ./linux.sh
 fi
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > ~/neobundle.sh
 sh ~/neobundle.sh
 rm ~/neobundle.sh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
---
+++ linux.sh
@@ -0,0 +1,22 @@
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update
+
+sudo apt-get install zsh vim-nox cmake build-essential qbittorrent calibre steam terminator php7.0 php7.0-common php7.0-cli composer spotify-client nodejs
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ~/Downloads/
+
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+
+sudo apt-get -f install
+
+mkdir -p ~/.config/terminator
+
+rm ~/.config/terminator/config
+
+ln -s ~/.dotfiles/config ~/.config/terminator/config
+
