6be59960345f9de25c1e9b20d61dce3bbdc98fe7
Codayblue <codayblue@gmail.com>
2017-05-19 19:11:42 -0700

    altered config since I went back to arch.

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ else
     sh ./linux.sh
 fi
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > ~/neobundle.sh
-sh ~/neobundle.sh
-rm ~/neobundle.sh
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+rm ~/.zshrc
+
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+rm -rf ~/.atom
+
 ln -s ~/.dotfiles/atom ~/.atom
--- linux.sh
+++ linux.sh
@@ -1,30 +1,6 @@
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo pacman -S --needed vlc calibre atom qbittorrent grsync rsync zsh jdk8-openjdk kotlin intellij-idea-community-edition gdm
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+yaourt -S --noconfirm --needed google-chrome discord
 
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-
-sudo apt-get update
-
-sudo apt-get install zsh vim-nox cmake build-essential qbittorrent calibre steam terminator php7.0 php7.0-common php7.0-cli composer spotify-client nodejs
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-wget https://github.com/atom/atom/releases/download/v1.16.0/atom-amd64.deb
-
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
-sudo dpkg -i atom-amd64.deb
-
-sudo apt-get -f install
-
-rm google-chrome-stable_current_amd64.deb
-
-rm atom-amd64.deb
-
-mkdir -p ~/.config/terminator
-
-rm ~/.config/terminator/config
-
-ln -s ~/.dotfiles/config ~/.config/terminator/config
-
+sudo systemctl disable lightdm
+sudo systemctl enable gdm
