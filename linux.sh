b181cdbde41068db68d8afcb2d14a1c3daa2daaf
codayblue <codayblue@gmail.com>
2017-05-24 22:44:55 -0700

    more stuff

--- linux.sh
+++ linux.sh
@@ -1,1 +1,9 @@
-sudo apt-get install zsh fonts-powerline steam tmux php composer php7.0-sqlite3 arc-theme php-common curl wget syncthing apt-transport-https
+sudo apt-get install zsh fonts-powerline steam tmux php composer php7.0-sqlite3 arc-theme php-common curl wget apt-transport-https
+
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+sudo apt-get update
+
+sudo apt-get install syncthing
