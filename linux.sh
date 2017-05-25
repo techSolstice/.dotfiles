e42b707ef75fea446ffc9eb9a5d697eb74083269
codayblue <codayblue@winterfell>
2017-05-24 22:32:27 -0700

    more packages needed when I install new systems

--- linux.sh
+++ linux.sh
@@ -1,1 +1,1 @@
-sudo apt-get install zsh fonts-powerline steam tmux php composer php7.0-sqlite3 arc-theme php-common curl wget
+sudo apt-get install zsh fonts-powerline steam tmux php composer php7.0-sqlite3 arc-theme php-common curl wget syncthing
