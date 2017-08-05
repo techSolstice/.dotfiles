d25a3252a007f41eedeb8136d9607053a8f1f08f
codayblue <codayblue@gmail.com>
2017-08-04 21:36:56 -0700

    Included xdebug

--- linux.sh
+++ linux.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install zsh fonts-powerline steam tmux curl wget apt-transport-https qbittorrent numlockx net-tools php7.0 php7.0-xml composer
+sudo apt-get -y install zsh fonts-powerline steam tmux curl wget apt-transport-https qbittorrent numlockx net-tools php7.0 php7.0-xml php7.0-sqlite3 php7.0-xdebug composer
 
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 
