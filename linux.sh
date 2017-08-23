4d6169245bdbfc007e948b831bba805a305b58d2
codayblue <codayblue@gmail.com>
2017-08-22 20:45:36 -0700

    Included more PHP extensions

--- linux.sh
+++ linux.sh
@@ -16,7 +16,15 @@ sudo add-apt-repository -y ppa:webupd8team/java
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-sudo apt-get -y install syncthing spotify-client php7.1-fpm php7.1 php7.1-xml php7.1-sqlite3 php7.1-xdebug php7.1-common nodejs pop-theme oracle-java8-installer
+sudo apt-get -y install syncthing spotify-client php7.1-fpm php7.1 php7.1-xml php7.1-sqlite3 php7.1-xdebug php7.1-common php7.1-mbstring php7.1-curl nodejs pop-theme oracle-java8-installer
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
+
+php -r "unlink('composer-setup.php');"
 
 sudo snap install discord
 
