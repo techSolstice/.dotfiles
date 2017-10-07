32dbf53be4e91b898b032b9931f4474fb491b1a1
codayblue <codayblue@gmail.com>
2017-10-07 09:09:30 -0700

    Included thunderbird

--- linux.sh
+++ linux.sh
@@ -11,7 +11,7 @@ fi
 sudo dnf config-manager -y --add-repo=https://negativo17.org/repos/fedora-spotify.repo
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf install -y steam spotify-client vim calibre arc-theme gnome-tweak-tool powerline-fonts zsh qbittorrent shutter php meld google-chrome docker docker-compose f26-backgrounds-animated composer php-pecl-xdebug wget screenfetch
+sudo dnf install -y steam spotify-client vim calibre arc-theme gnome-tweak-tool powerline-fonts zsh qbittorrent shutter php meld google-chrome docker docker-compose f26-backgrounds-animated composer php-pecl-xdebug wget screenfetch thunderbird
 
 cd /tmp && rm -rf Flat-Remix && git clone https://github.com/daniruiz/Flat-Remix && mkdir -p ~/.icons && cp -r "Flat-Remix/Flat Remix" ~/.icons
 
