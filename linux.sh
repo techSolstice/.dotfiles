3961cde274d90f2e2ee2c73a2056a6e0a69564ee
codayblue <codayblue@gmail.com>
2017-10-30 20:52:57 -0700

    included missing software I use everyday

--- linux.sh
+++ linux.sh
@@ -8,9 +8,15 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 # PHP updated repo
 sudo add-apt-repository -y ppa:ondrej/php
 
+# Setup nodejs
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
 # Read the repos
 sudo apt-get update -y
 
 # install all the stuff
-sudo apt-get install build-essential fonts-powerline zsh meld steam calibre curl vim-nox qbittorrent php7.1-fpm php7.1 php7.1-common php7.1-curl php7.1-dom php7.1-xdebug php7.1-mbstring spotify-client
+sudo apt-get install build-essential fonts-powerline zsh meld steam calibre curl vim-nox qbittorrent php7.1-fpm php7.1 php7.1-common php7.1-curl php7.1-dom php7.1-xdebug php7.1-mbstring spotify-client apt-transport-https ca-certificates software-properties-common nodejs
+
+# Install docker
+cd /tmp && curl -o get-docker.sh get.docker.com && sudo bash ./get-docker.sh
 
