261da9586ad5324515813c273ec527d67797fd11
codayblue <codayblue@gmail.com>
2017-11-04 23:16:55 -0700

    Configured script for arch linux

--- install.sh
+++ install.sh
@@ -16,12 +16,11 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 rm ~/.zshrc 2>&1 > /dev/null
 
 mkdir -p ~/.vim/bundle
-mkdir -p ~/.config/nvim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc 2>&1 > /dev/null
+ln -s ~/.dotfiles/.vimrc ~/.vimrc 2>&1 > /dev/null
 
 chsh -s /bin/zsh
 
--- linux.sh
+++ linux.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-# SPOTIFY
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+# Install all the apps I need
+sudo pacman --needed --noconfirm -S php rsync syncthing calibre qbittorrent gnome-tweak-tool docker docker-compose chrome-gnome-shell git vim meld zsh
 
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# Include a key that is needed for discord below
+gpg --recv-keys 0FC3042E345AD05D
 
-# PHP updated repo
-sudo add-apt-repository -y ppa:ondrej/php
-
-# Setup nodejs
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-
-# Read the repos
-sudo apt-get update -y
-
-# install all the stuff
-sudo apt-get install build-essential fonts-powerline zsh meld steam calibre curl vim-nox qbittorrent php7.1-fpm php7.1 php7.1-common php7.1-curl php7.1-dom php7.1-xdebug php7.1-mbstring spotify-client apt-transport-https ca-certificates software-properties-common nodejs
-
-# Install docker
-cd /tmp && curl -o get-docker.sh get.docker.com && sudo bash ./get-docker.sh
-
+# Install 3rd party apps
+yaourt -S --noconfirm --needed google-chrome spotify discord
