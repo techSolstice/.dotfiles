0bc76bb23afe63202d844e5744395556dcb6d743
codayblue <codayblue@gmail.com>
2017-12-24 06:32:58 -0800

    Updated install script. removed osx install.

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [ `uname -s` = 'Darwin' ]; then
-    ./osx.sh
-else
-    ./linux.sh
-fi
+# Install all the apps I need
+sudo pacman --needed --noconfirm -S php rsync syncthing calibre qbittorrent docker docker-compose xfce4-goodies git vim meld zsh curl arc-gtk-theme papirus-icon-theme steam powerline-fonts
+
+# Include a key that is needed for discord below
+gpg --recv-keys 8F0871F202119294
+
+# Install AUR apps
+yaourt -S --noconfirm --needed google-chrome spotify discord visual-studio-code-bin slack-desktop
+
+wget -O /tmp/pia_nm.sh  https://www.privateinternetaccess.com/installer/pia-nm.sh
+sudo chmod +x /tmp/pia_nm.sh
+sudo /tmp/pia_nm.sh
 
 git config --global user.email $1
 git config --global user.name $2
@@ -15,6 +22,7 @@ git config --global core.excludesfile ~/.dotfiles/.gitignore_global
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
 rm ~/.zshrc 2>&1 > /dev/null
+rm ~/.vimrc 2>&1 > /dev/null
 
 mkdir -p ~/.vim/bundle
 
--- osx.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew cask install calibre java meld virtualbox iterm2
-brew install zsh cmake syncthing editorconfig composer php71 wget
